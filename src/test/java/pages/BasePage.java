2d2d3067a780774a9091e7311f75e6b2ca6ddc78
Nir Tal <nirt236@gmail.com>
2021-06-06 09:43:35 +0300

    added basePage

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+
+public abstract class BasePage {
+    @Autowired
+    private WebDriver driver;
+
+    @PostConstruct
+    public void initPage()
+    {
+        PageFactory.initElements(driver, this);
+    }
+}
