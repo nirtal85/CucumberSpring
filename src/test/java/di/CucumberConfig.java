903842093707e7efbd7d27ebd992bf2f92b65296
Nir Tal <nirt236@gmail.com>
2021-06-07 09:07:42 +0300

    condition on prop

--- src/test/java/di/CucumberConfig.java
+++ src/test/java/di/CucumberConfig.java
@@ -3,6 +3,8 @@ package di;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
@@ -13,8 +15,17 @@ public class CucumberConfig {
 
     @Bean(destroyMethod = "quit")
     @Scope(SCOPE_CUCUMBER_GLUE)
-    public WebDriver getDriver() {
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
+    public WebDriver getChromeDriver() {
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
+    }
+
+    @Bean(destroyMethod = "quit")
+    @Scope(SCOPE_CUCUMBER_GLUE)
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public WebDriver getFirefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
     }
 }
