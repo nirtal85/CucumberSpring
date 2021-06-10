7c99e7738ed77d608779536183a218b1ba4121e9
Nir Tal <nirt236@gmail.com>
2021-06-11 00:39:49 +0300

    screen record

--- src/test/java/di/CucumberConfig.java
+++ src/test/java/di/CucumberConfig.java
@@ -1,6 +1,10 @@
 package di;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.monte.media.Format;
+import org.monte.media.FormatKeys;
+import org.monte.media.math.Rational;
+import org.monte.screenrecorder.ScreenRecorder;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +14,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
+import static org.monte.media.FormatKeys.*;
+import static org.monte.media.VideoFormatKeys.*;
 
 public class CucumberConfig {
 
@@ -28,5 +38,37 @@ public class CucumberConfig {
     public WebDriver getFirefoxDriver() {
         WebDriverManager.firefoxdriver().setup();
         return new FirefoxDriver();
+    }
+
+    /**
+     *         initialize the screen recorder:
+     *         default graphics configuration
+     *         full screen recording
+     *         record in AVI format
+     *         15 frames per second
+     *         black mouse pointer
+     *         no audio
+     *         save capture to predefined location
+     * @return screen recorder
+     */
+    @Bean
+    public ScreenRecorder screenRecorder() throws IOException, AWTException {
+        // set the graphics configuration
+        System.setProperty("java.awt.headless","false");
+        GraphicsConfiguration gc = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice()
+                .getDefaultConfiguration();
+        return new ScreenRecorder(gc,
+                gc.getBounds(),
+                new Format(MediaTypeKey, FormatKeys.MediaType.FILE, MimeTypeKey, MIME_AVI),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        DepthKey, 24, FrameRateKey, Rational.valueOf(15),
+                        QualityKey, 1.0f,
+                        KeyFrameIntervalKey, 15 * 60),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey, Rational.valueOf(30)),
+                null,
+                new File(System.getProperty("user.dir") + "/target" + "/video"));
     }
 }
