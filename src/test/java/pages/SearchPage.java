1b3068d6022159368587028d3e1c6c8497c7b1bd
Nir Tal <nirt236@gmail.com>
2021-06-26 22:13:38 +0300

    updated page scope

--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -1,14 +1,17 @@
 package pages;
 
+import io.cucumber.spring.ScenarioScope;
 import lombok.Getter;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+
+@ScenarioScope
 @Component
-@Getter
 @Lazy
+@Getter
 public class SearchPage extends BasePage {
 
     @FindBy(css = "q")
