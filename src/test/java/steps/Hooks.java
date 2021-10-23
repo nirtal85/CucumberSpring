da3bdc4f6cc00c6e69168ca82e9f79c9d0a3b075
Nir Tal <nirt236@gmail.com>
2021-10-24 00:13:37 +0300

    beforeall

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -2,6 +2,7 @@ package steps;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
 import org.monte.screenrecorder.ScreenRecorder;
 import org.openqa.selenium.OutputType;
@@ -37,7 +38,8 @@ public class Hooks extends BaseSteps {
     @Autowired
     private ScreenRecorder screenRecorder;
 
-    static {
+    @BeforeAll
+    public static void beforeAll() {
         try {
             Files.createDirectories(Paths.get(VIDEO_PATH));
         } catch (IOException e) {
