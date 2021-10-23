6c1129b7e28529bfc9d4839d6667446f26277405
Nir Tal <nirt236@gmail.com>
2021-10-24 00:08:45 +0300

    removed allure

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "classpath:features",
         tags = "not @Ignore",
-        plugin = "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
         glue = "steps"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
